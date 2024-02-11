3bbbc3cefe6b07e95a3c9619bc4fea918f692af6
Coqire <megen.horne@hotmail.com>
2024-02-12 04:00:19 +1100

    - Added halters into the game

--- src/main/java/com/coqire/bagesequineyaddon/item/ModItems.java
+++ src/main/java/com/coqire/bagesequineyaddon/item/ModItems.java
@@ -26,6 +26,18 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
     public static final RegistryObject<Item> HALTER_1 = ITEMS.register("halter_1",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<Item> HALTER_2 = ITEMS.register("halter_2",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<Item> HALTER_3 = ITEMS.register("halter_3",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<Item> HALTER_4 = ITEMS.register("halter_4",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<Item> HALTER_5 = ITEMS.register("halter_5",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<Item> HALTER_6 = ITEMS.register("halter_6",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<Item> HALTER_7 = ITEMS.register("halter_7",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
 
 
     public static void register(IEventBus eventBus) {
