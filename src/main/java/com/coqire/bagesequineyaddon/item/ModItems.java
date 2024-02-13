ae0d3164529b4c8c5be7725351533b9890013512
Coqire <megen.horne@hotmail.com>
2024-02-13 18:09:50 +1100

    - Rendered halters in game now functional

--- src/main/java/com/coqire/bagesequineyaddon/item/ModItems.java
+++ src/main/java/com/coqire/bagesequineyaddon/item/ModItems.java
@@ -36,9 +36,42 @@ public class ModItems {
             () -> new HalterItem("halter_5", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
     public static final RegistryObject<HalterItem> HALTER_6 = ITEMS.register("halter_6",
             () -> new HalterItem("halter_6", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
-
     public static final RegistryObject<HalterItem> HALTER_7 = ITEMS.register("halter_7",
             () -> new HalterItem("halter_7", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<HalterItem> halter_fluffy_red = ITEMS.register("halter_fluffy_red",
+            () -> new HalterItem("halter_fluffy_red", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<HalterItem> halter_fluffy_orange = ITEMS.register("halter_fluffy_orange",
+            () -> new HalterItem("halter_fluffy_orange", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<HalterItem> halter_fluffy_yellow = ITEMS.register("halter_fluffy_yellow",
+            () -> new HalterItem("halter_fluffy_yellow", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<HalterItem> halter_fluffy_lime = ITEMS.register("halter_fluffy_lime",
+            () -> new HalterItem("halter_fluffy_lime", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<HalterItem> halter_fluffy_green = ITEMS.register("halter_fluffy_green",
+            () -> new HalterItem("halter_fluffy_green", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<HalterItem> halter_fluffy_cyan = ITEMS.register("halter_fluffy_cyan",
+            () -> new HalterItem("halter_fluffy_cyan", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<HalterItem> halter_fluffy_light_blue = ITEMS.register("halter_fluffy_light_blue",
+            () -> new HalterItem("halter_fluffy_light_blue", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<HalterItem> halter_fluffy_blue = ITEMS.register("halter_fluffy_blue",
+            () -> new HalterItem("halter_fluffy_blue", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<HalterItem> halter_fluffy_purple = ITEMS.register("halter_fluffy_purple",
+            () -> new HalterItem("halter_fluffy_purple", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<HalterItem> halter_fluffy_magetna = ITEMS.register("halter_fluffy_magenta",
+            () -> new HalterItem("halter_fluffy_magenta", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<HalterItem> halter_fluffy_pink = ITEMS.register("halter_fluffy_pink",
+            () -> new HalterItem("halter_fluffy_pink", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<HalterItem> halter_fluffy_white = ITEMS.register("halter_fluffy_white",
+            () -> new HalterItem("halter_fluffy_white", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<HalterItem> halter_fluffy_brown = ITEMS.register("halter_fluffy_brown",
+            () -> new HalterItem("halter_fluffy_brown", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<HalterItem> halter_fluffy_light_gray = ITEMS.register("halter_fluffy_light_gray",
+            () -> new HalterItem("halter_fluffy_light_gray", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<HalterItem> halter_fluffy_gray = ITEMS.register("halter_fluffy_gray",
+            () -> new HalterItem("halter_fluffy_gray", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+    public static final RegistryObject<HalterItem> halter_fluffy_black = ITEMS.register("halter_fluffy_black",
+            () -> new HalterItem("halter_fluffy_black", new Item.Properties().tab(ModCreativeModeTab.BAGEKSADDON_TAB)));
+
+
 
 
     public static void register(IEventBus eventBus) {
