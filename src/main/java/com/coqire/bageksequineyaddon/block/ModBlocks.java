b0f0e5af9f47c7971b59a02fcae2150cf3a4f7d0
Coqire <megen.horne@hotmail.com>
2024-03-17 23:29:12 +1100

    - Added rest of the deepslate ores

--- src/main/java/com/coqire/bageksequineyaddon/block/ModBlocks.java
+++ src/main/java/com/coqire/bageksequineyaddon/block/ModBlocks.java
@@ -32,10 +32,16 @@ public class ModBlocks {
     public static final RegistryObject<Block> EQUIUM_ORE = registerBlock("equium_ore",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(5f).requiresCorrectToolForDrops()), BageksAddonCreativeModeTab.BAGEKSADDON_TAB);
+    public static final RegistryObject<Block> DEEPSLATE_EQUIUM_ORE = registerBlock("deepslate_equium_ore",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(5f).requiresCorrectToolForDrops()), BageksAddonCreativeModeTab.BAGEKSADDON_TAB);
     public static final RegistryObject<Block> EQUIUM_BLOCK = registerBlock("equium_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
                     .strength(7f).requiresCorrectToolForDrops()), BageksAddonCreativeModeTab.BAGEKSADDON_TAB);
     public static final RegistryObject<Block> CORALARITE_ORE = registerBlock("coralarite_ore",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(5f).requiresCorrectToolForDrops()), BageksAddonCreativeModeTab.BAGEKSADDON_TAB);
+    public static final RegistryObject<Block> DEEPSLATE_CORALARITE_ORE = registerBlock("deepslate_coralarite_ore",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(5f).requiresCorrectToolForDrops()), BageksAddonCreativeModeTab.BAGEKSADDON_TAB);
     public static final RegistryObject<Block> CORALARITE_BLOCK = registerBlock("coralarite_block",
