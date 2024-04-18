feba867c396f882dbf1eaadd0eb3aaa1a689400e
Coqire <megen.horne@hotmail.com>
2024-04-18 20:39:15 +1000

    - Readded pastel saddle pads

--- src/main/java/com/coqire/bageksequineyaddon/registry/BageksPastelTack.java
+++ src/main/java/com/coqire/bageksequineyaddon/registry/BageksPastelTack.java
@@ -1,10 +1,7 @@
 package com.coqire.bageksequineyaddon.registry;
 
 import com.alaharranhonor.swem.forge.items.SWEMHorseArmorItem;
-import com.alaharranhonor.swem.forge.items.tack.EnglishBreastCollar;
-import com.alaharranhonor.swem.forge.items.tack.EnglishLegWraps;
-import com.alaharranhonor.swem.forge.items.tack.HalterItem;
-import com.alaharranhonor.swem.forge.items.tack.PastureBlanketItem;
+import com.alaharranhonor.swem.forge.items.tack.*;
 import com.coqire.bageksequineyaddon.item.BageksAddonCreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -24,6 +21,7 @@ public class BageksPastelTack {
     public static final List<RegistryObject<EnglishLegWraps>> ENGLISH_LEG_WRAPS_BAGEK_PASTEL;
     public static final List<RegistryObject<PastureBlanketItem>> PASTURE_BLANKET_BAGEK_PASTEL;
     public static final List<RegistryObject<EnglishBreastCollar>> QUARTERSHEET_BAGEK_PASTEL;
+    public static final List<RegistryObject<EnglishBlanketItem>> ENGLISH_BLANKET_BAGEK_PASTEL;
 
     public static void init(IEventBus eventBus) {
         ITEMS.register(eventBus);
@@ -36,6 +34,7 @@ public class BageksPastelTack {
         ENGLISH_LEG_WRAPS_BAGEK_PASTEL = new ArrayList ();
         PASTURE_BLANKET_BAGEK_PASTEL = new ArrayList ();
         QUARTERSHEET_BAGEK_PASTEL = new ArrayList();
+        ENGLISH_BLANKET_BAGEK_PASTEL = new ArrayList();
 
         int var1 = 7;
         var rContext = new Object() {
@@ -62,6 +61,11 @@ public class BageksPastelTack {
 
             QUARTERSHEET_BAGEK_PASTEL.add(ITEMS.register("quartersheet_bagek_p" + counter, () ->
                     new EnglishBreastCollar("quartersheet_bagek_p" + counter, (new Item.Properties())
+                            .tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16))
+            ));
+
+            ENGLISH_BLANKET_BAGEK_PASTEL.add(ITEMS.register("english_blanket_bagek_p" + counter, () ->
+                    new EnglishBlanketItem("english_blanket_bagek_p" + counter, (new Item.Properties())
                             .tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16))
             ));
 
