575b68d3a7468c6b1757cc2570375c0a0fee5889
Coqire <megen.horne@hotmail.com>
2024-04-24 00:39:23 +1000

    - Added a new breastcollar type

--- src/main/java/com/coqire/bageksequineyaddon/registry/BageksMainTack.java
+++ src/main/java/com/coqire/bageksequineyaddon/registry/BageksMainTack.java
@@ -20,55 +20,67 @@ public class BageksMainTack {
 
     // ENGLISH SADDLES
     public static final RegistryObject<EnglishSaddleItem> english_saddle_xc_black = ITEMS.register("english_saddle_xc_black",
-            () -> new EnglishSaddleItem("english_saddle_xc_black", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
+            () -> new EnglishSaddleItem("english_saddle_xc_black", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
     public static final RegistryObject<EnglishSaddleItem> english_saddle_xc_brown = ITEMS.register("english_saddle_xc_brown",
-            () -> new EnglishSaddleItem("english_saddle_xc_brown", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
+            () -> new EnglishSaddleItem("english_saddle_xc_brown", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
 
     // ENGLISH BRIDLES
     public static final RegistryObject<EnglishBridleItem> english_bridle_xc_black = ITEMS.register("english_bridle_xc_black",
-            () -> new EnglishBridleItem("english_bridle_xc_black", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
+            () -> new EnglishBridleItem("english_bridle_xc_black", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
     public static final RegistryObject<EnglishBridleItem> english_bridle_xc_fluffy_black = ITEMS.register("english_bridle_xc_fluffy_black",
-            () -> new EnglishBridleItem("english_bridle_xc_fluffy_black", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
+            () -> new EnglishBridleItem("english_bridle_xc_fluffy_black", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
     public static final RegistryObject<EnglishBridleItem> english_bridle_xc_brown = ITEMS.register("english_bridle_xc_brown",
-            () -> new EnglishBridleItem("english_bridle_xc_brown", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
+            () -> new EnglishBridleItem("english_bridle_xc_brown", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
     public static final RegistryObject<EnglishBridleItem> english_bridle_xc_fluffy_brown = ITEMS.register("english_bridle_xc_fluffy_brown",
-            () -> new EnglishBridleItem("english_bridle_xc_fluffy_brown", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
+            () -> new EnglishBridleItem("english_bridle_xc_fluffy_brown", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
 
     // ENGLISH GIRTHS
     public static final RegistryObject<EnglishGirthStrap> english_girth_strap_xc_black = ITEMS.register("english_girth_strap_xc_black",
-            () -> new EnglishGirthStrap("english_girth_strap_xc_black", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
+            () -> new EnglishGirthStrap("english_girth_strap_xc_black", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
     public static final RegistryObject<EnglishGirthStrap> english_girth_strap_xc_fluffy_black = ITEMS.register("english_girth_strap_xc_fluffy_black",
-            () -> new EnglishGirthStrap("english_girth_strap_xc_fluffy_black", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
+            () -> new EnglishGirthStrap("english_girth_strap_xc_fluffy_black", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
     public static final RegistryObject<EnglishGirthStrap> english_girth_strap_xc_brown = ITEMS.register("english_girth_strap_xc_brown",
-            () -> new EnglishGirthStrap("english_girth_strap_xc_brown", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
+            () -> new EnglishGirthStrap("english_girth_strap_xc_brown", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
     public static final RegistryObject<EnglishGirthStrap> english_girth_strap_xc_fluffy_brown = ITEMS.register("english_girth_strap_xc_fluffy_brown",
-            () -> new EnglishGirthStrap("english_girth_strap_xc_fluffy_brown", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
+            () -> new EnglishGirthStrap("english_girth_strap_xc_fluffy_brown", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
 
     // ENGLISH BREAST COLLARS
-    public static final RegistryObject<BreastCollarItem> english_breast_collar_xc_black = ITEMS.register("english_breast_collar_xc_black",
-            () -> new BreastCollarItem("english_breast_collar_xc_black", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
-    public static final RegistryObject<BreastCollarItem> english_breast_collar_xc_fluffy_black = ITEMS.register("english_breast_collar_xc_fluffy_black",
-            () -> new BreastCollarItem("english_breast_collar_xc_fluffy_black", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
-    public static final RegistryObject<BreastCollarItem> english_breast_collar_freemotion_black = ITEMS.register("english_breast_collar_freemotion_black",
-            () -> new BreastCollarItem("english_breast_collar_freemotion_black", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
-    public static final RegistryObject<BreastCollarItem> english_breast_collar_xc_brown = ITEMS.register("english_breast_collar_xc_brown",
-            () -> new BreastCollarItem("english_breast_collar_xc_brown", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
-    public static final RegistryObject<BreastCollarItem> english_breast_collar_xc_fluffy_brown = ITEMS.register("english_breast_collar_xc_fluffy_brown",
-            () -> new BreastCollarItem("english_breast_collar_xc_fluffy_brown", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
-    public static final RegistryObject<BreastCollarItem> english_breast_collar_freemotion_brown = ITEMS.register("english_breast_collar_freemotion_brown",
-            () -> new BreastCollarItem("english_breast_collar_freemotion_brown", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
+    public static final RegistryObject<EnglishBreastCollar> english_breast_collar_xc_black = ITEMS.register("english_breast_collar_xc_black",
+            () -> new EnglishBreastCollar("english_breast_collar_xc_black", new Item.Properties()
+                    .tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
+    public static final RegistryObject<EnglishBreastCollar> english_breast_collar_xc_fluffy_black = ITEMS.register("english_breast_collar_xc_fluffy_black",
+            () -> new EnglishBreastCollar("english_breast_collar_xc_fluffy_black", new Item.Properties()
+                    .tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
+    public static final RegistryObject<EnglishBreastCollar> english_breast_collar_freemotion_black = ITEMS.register("english_breast_collar_freemotion_black",
+            () -> new EnglishBreastCollar("english_breast_collar_freemotion_black", new Item.Properties().
+                    tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
+    public static final RegistryObject<EnglishBreastCollar> english_breast_collar_xc_brown = ITEMS.register("english_breast_collar_xc_brown",
+            () -> new EnglishBreastCollar ("english_breast_collar_xc_brown", new Item.Properties().
+                    tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
+    public static final RegistryObject<EnglishBreastCollar> english_breast_collar_xc_fluffy_brown = ITEMS.register("english_breast_collar_xc_fluffy_brown",
+            () -> new EnglishBreastCollar("english_breast_collar_xc_fluffy_brown", new Item.Properties().
+                    tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
+    public static final RegistryObject<EnglishBreastCollar> english_breast_collar_freemotion_brown = ITEMS.register("english_breast_collar_freemotion_brown",
+            () -> new EnglishBreastCollar("english_breast_collar_freemotion_brown", new Item.Properties().
+                    tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
+    public static final RegistryObject<EnglishBreastCollar> ENGLISH_BREAST_COLLAR_ENGLISH_BLACK = ITEMS.register("english_breast_collar_english_black",
+            () -> new EnglishBreastCollar("english_breast_collar_english_black", new Item.Properties()
+                    .tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
+    public static final RegistryObject<EnglishBreastCollar> ENGLISH_BREAST_COLLAR_ENGLISH_BROWN = ITEMS.register("english_breast_collar_english_brown",
+            () -> new EnglishBreastCollar("english_breast_collar_english_brown", new Item.Properties().
+                    tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
 
 
     // TRAVEL BOOTS
     public static final RegistryObject<EnglishLegWraps> english_leg_wraps_travel_bagek_1 = ITEMS.register("english_leg_wraps_travel_bagek_1",
-            () -> new EnglishLegWraps("english_leg_wraps_travel_bagek_1", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
+            () -> new EnglishLegWraps("english_leg_wraps_travel_bagek_1", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
     public static final RegistryObject<EnglishLegWraps> english_leg_wraps_travel_bagek_4 = ITEMS.register("english_leg_wraps_travel_bagek_4",
-            () -> new EnglishLegWraps("english_leg_wraps_travel_bagek_4", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
+            () -> new EnglishLegWraps("english_leg_wraps_travel_bagek_4", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
     public static final RegistryObject<EnglishLegWraps> english_leg_wraps_travel_bagek_8 = ITEMS.register("english_leg_wraps_travel_bagek_8",
-            () -> new EnglishLegWraps("english_leg_wraps_travel_bagek_8", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
+            () -> new EnglishLegWraps("english_leg_wraps_travel_bagek_8", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
     public static final RegistryObject<EnglishLegWraps> english_leg_wraps_travel_bagek_9 = ITEMS.register("english_leg_wraps_travel_bagek_9",
-            () -> new EnglishLegWraps("english_leg_wraps_travel_bagek_9", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
+            () -> new EnglishLegWraps("english_leg_wraps_travel_bagek_9", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
     public static final RegistryObject<EnglishLegWraps> english_leg_wraps_travel_bagek_10 = ITEMS.register("english_leg_wraps_travel_bagek_10",
-            () -> new EnglishLegWraps("english_leg_wraps_travel_bagek_10", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(4)));
+            () -> new EnglishLegWraps("english_leg_wraps_travel_bagek_10", new Item.Properties().tab(BageksAddonCreativeModeTab.BAGEKSADDON_TAB).stacksTo(16)));
 
 }
