d1c0d23ec0d63f21330314c288bdf44e21b9bf2a
Coqire <megen.horne@hotmail.com>
2024-05-01 18:30:40 +1000

    - Added a search tab

--- src/main/java/com/coqire/bageksequineyaddon/item/BageksAddonCreativeModeTab.java
+++ src/main/java/com/coqire/bageksequineyaddon/item/BageksAddonCreativeModeTab.java
@@ -1,5 +1,6 @@
 package com.coqire.bageksequineyaddon.item;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
@@ -9,5 +10,10 @@ public class BageksAddonCreativeModeTab {
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.BAGUETTE.get());
         }
-    };
-}
+        @Override
+        public boolean hasSearchBar() { return true; }
+
+    }.setBackgroundImage(new ResourceLocation("minecraft","textures/gui/container/creative_inventory/tab_item_search.png"));
+
+};
+
