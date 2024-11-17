baa2c1ae1b8003b31339342b7a21dc7b19ea3c44
Coqire <megen.horne@hotmail.com>
2024-11-17 20:04:37 +1100

    - Fixed pickaxe tiers

--- src/main/java/com/coqire/bageksequineyaddon/item/ModTiers.java
+++ src/main/java/com/coqire/bageksequineyaddon/item/ModTiers.java
@@ -9,9 +9,9 @@ public class ModTiers {
             ,2.0f,14, BlockTags.NEEDS_IRON_TOOL,
             () -> Ingredient.of(ModItems.SUNBURST.get()));
     public static final ForgeTier EQUIUM = new ForgeTier(3,1400,8.0f
-            ,2.0f,10, BlockTags.NEEDS_DIAMOND_TOOL,
+            ,2.0f,10, BlockTags.NEEDS_IRON_TOOL,
             () -> Ingredient.of(ModItems.equium.get()));
-    public static final ForgeTier CORALARITE = new ForgeTier(3,800,7.0f
-            ,2.0f,12, BlockTags.NEEDS_DIAMOND_TOOL,
+    public static final ForgeTier CORALARITE = new ForgeTier(3,1400,8.0f
+            ,2.0f,10, BlockTags.NEEDS_IRON_TOOL,
             () -> Ingredient.of(ModItems.coralarite.get()));
 }
